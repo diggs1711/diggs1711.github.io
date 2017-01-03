9ed80605e82dac2fc98d5226fdad142ad7380550
Higgins <dahiggins@deloitte.ie>
2017-01-03 16:41:51 +0000

    calcualtor

--- matched betting/calculator.js
+++ matched betting/calculator.js
@@ -1,20 +1,42 @@
+//Inputs
 var stake = 0;
 var StakeForfeit = false;
 
 var backOdds = 0;
-var backReturn = 0;
 
 var layOdds = 0;
 var layCommission = 0;
+
+//Outputs
+var backReturn = 0;
+
 var layRisk = 0;
 var layStake = 0;
 
 var backProfit = 0;
 var layProfit = 0;
 
-function calculateBackReturn(s, bOdds, backCommission, forfeit) {
+var calcLay = document.getElementsByClassName('calculate-lay-button');
+calcLay[0].addEventListener('click', function(e) {
+	stake = document.getElementById('stake').value;
+	backOdds = document.getElementById('back-odds').value;
+	layOdds = document.getElementById('lay-odds').value;
+	layCommission = document.getElementById('lay-commission').value;
+
+	backReturn = calculateBackReturn(stake, backOdds, 0, false);
+	layStake = calculateLayStake(backReturn, layOdds, layCommission);
+	layRisk = calculateLayRisk(layStake, layOdds);
+
+	alert(layStake);
+	alert(layRisk);
+	document.getElementsByClassName('lay-stake')[0].innerHTML = "Lay stake is " + layStake;
+	console.log(calculateLayProfit());
+	console.log(calculateBackProfit());
+})
+
+function calculateBackReturn(stk, bOdds, backCommission, forfeit) {
 	StakeForfeit = forfeit;
-	stake = s;
+	stake = stk;
 	backOdds = bOdds;
 
 	return backOdds*stake - (backCommission*((backOdds - 1)* stake)/100) - isStakeForfeit();
@@ -39,7 +61,7 @@ function calculateLayStake(bReturns, lOdds, lCommission) {
 	layCommission = lCommission;
 
 	return backReturn/(layOdds-(layCommission/100));
-} 
+}
 
 function calculateBackProfit() {
 	return backReturn - layRisk;
@@ -48,10 +70,3 @@ function calculateBackProfit() {
 function calculateLayProfit() {
 	return layStake*(100- layCommission)/100;
 }
-
-backReturn = calculateBackReturn(25, 15, 0, true);
-layStake = calculateLayStake(backReturn, 15, 5);
-layRisk = calculateLayRisk(layStake, layOdds);
-
-console.log(calculateLayProfit());
-console.log(calculateBackProfit());
