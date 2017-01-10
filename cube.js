e24244bf2d33923fa11f31e4065f5684a53c374f
David <davidhiggins1712@gmail.com>
2017-01-10 23:08:47 +0000

    webgl

--- cube.js
+++ cube.js
@@ -2,23 +2,23 @@ var canvas;
 var gl;
 
 function start() {
-  canvas = document.getElementById("glcanvas");
+    canvas = document.getElementById("glcanvas");
 
-  initWebGL(canvas);      // Initialize the GL context
+    initWebGL(canvas); // Initialize the GL context
 
-  // Only continue if WebGL is available and working
+    // Only continue if WebGL is available and working
 
-  if (gl) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Set clear color to black, fully opaque
-    gl.clearDepth(1.0);                 // Clear everything
-    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-    gl.depthFunc(gl.LEQUAL);      // Near things obscure far things
-  
-    initShaders();
-    initBuffers();
+    if (gl) {
+        gl.clearColor(0.0, 0.0, 0.0, 1.0); // Set clear color to black, fully opaque
+        gl.clearDepth(1.0); // Clear everything
+        gl.enable(gl.DEPTH_TEST); // Enable depth testing
+        gl.depthFunc(gl.LEQUAL); // Near things obscure far things
 
-    setInterval(drawScene, 15)
-  }
+        initShaders();
+        initBuffers();
+
+        setInterval(drawScene, 15)
+    }
 }
 
 //
@@ -28,130 +28,124 @@ function start() {
 // WebGL isn't available or could not be initialized.
 //
 function initWebGL() {
-  gl = null;
+    gl = null;
 
-  try {
-    gl = canvas.getContext("experimental-webgl");
-  }
-  catch(e) {
-  }
+    try {
+        gl = canvas.getContext("experimental-webgl");
+    } catch (e) {}
 
-  // If we don't have a GL context, give up now
+    // If we don't have a GL context, give up now
 
-  if (!gl) {
-    alert("Unable to initialize WebGL. Your browser may not support it.");
-  }
+    if (!gl) {
+        alert("Unable to initialize WebGL. Your browser may not support it.");
+    }
 }
 
 function initShaders() {
-	var fragmentShader = getShader(gl, "shader-fs");
-	var vertexShader = getShader(gl, "shader-vs");
+    var fragmentShader = getShader(gl, "shader-fs");
+    var vertexShader = getShader(gl, "shader-vs");
 
-	console.log(fragmentShader, vertexShader)
-	shaderProgram = gl.createProgram();
-	gl.attachShader(shaderProgram, vertexShader);
-	gl.attachShader(shaderProgram, fragmentShader);
-	gl.linkProgram(shaderProgram);
+    console.log(fragmentShader, vertexShader)
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
 
-	if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)){
-		console.log("unable tp initialize shaderProgram");
-	}
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        console.log("unable tp initialize shaderProgram");
+    }
 
-	gl.useProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
 
-	vertextPositionAttribute = gl.getAttribLocation(shaderProgram,"aVertexPosition");
-	gl.enableVertexAttribArray(vertextPositionAttribute);
+    vertextPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(vertextPositionAttribute);
 }
 
 function getShader(gl, id, type) {
-	var shaderScript, theSource, currentChild, shader;
+    var shaderScript, theSource, currentChild, shader;
 
-	shaderScript = document.getElementById(id);
+    shaderScript = document.getElementById(id);
 
 
-	if(!shaderScript)
-		return null;
+    if (!shaderScript)
+        return null;
 
-	theSource = shaderScript.text;
+    theSource = shaderScript.text;
 
-	if(!type){
-		if(shaderScript.type === "x-shader/x-fragment"){
-			type = gl.FRAGMENT_SHADER;
-		}
-		else if (shaderScript.type === "x-shader/x-vertex"){
-			type = gl.VERTEX_SHADER;
-		}
-		else{
-			return null;
-		}
-}
-	shader = gl.createShader(type);
+    if (!type) {
+        if (shaderScript.type === "x-shader/x-fragment") {
+            type = gl.FRAGMENT_SHADER;
+        } else if (shaderScript.type === "x-shader/x-vertex") {
+            type = gl.VERTEX_SHADER;
+        } else {
+            return null;
+        }
+    }
+    shader = gl.createShader(type);
 
-	gl.shaderSource(shader, theSource);
+    gl.shaderSource(shader, theSource);
 
-	gl.compileShader(shader);
-	console.log(shader)
-	if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
-		console.log("Error compiling the shaders")
-		gl.deleteShader(shader);
-		return null;
-}
+    gl.compileShader(shader);
+    console.log(shader)
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.log("Error compiling the shaders")
+        gl.deleteShader(shader);
+        return null;
+    }
 
-	return shader;
+    return shader;
 }
 
-var horizAspect = 480/640;
+var horizAspect = 480 / 640;
 
 function initBuffers() {
-	squareVerticesBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+    squareVerticesBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
 
-	var vertices = [
-	1.0, 1.0, 0.0,
-	-1.0, 1.0, 0.0,
-	1.0, -1.0, 0.0,
-	-1.0, -1.0, 0.0
-	];
+    var vertices = [
+        1.0, 1.0, 0.0, -1.0, 1.0, 0.0,
+        1.0, -1.0, 0.0, -1.0, -1.0, 0.0
+    ];
 
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
 }
 
 function drawScene() {
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	perspectiveMatrix = makePerspective(45, 640.0/480.0, 0.1, 100.0);
+    perspectiveMatrix = makePerspective(45, 640.0 / 480.0, 0.1, 100.0);
 
-	loadIdentity();
-	mvTranslate([-0.0, 0.0, -6.0])
+    loadIdentity();
+    mvTranslate([-0.0, 0.0, -6.0])
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer)
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer)
 
-	gl.vertexAttribPointer(vertextPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-	setMatrixUniforms();
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    gl.vertexAttribPointer(vertextPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+    setMatrixUniforms();
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
 
 function loadIdentity() {
-  mvMatrix = Matrix.I(4);
+    mvMatrix = Matrix.I(4);
 }
 
 function multMatrix(m) {
-  mvMatrix = mvMatrix.x(m);
+    mvMatrix = mvMatrix.x(m);
 }
 
 function mvTranslate(v) {
-  multMatrix(Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4());
+    multMatrix(Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4());
 }
 
 function setMatrixUniforms() {
-  var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
-  gl.uniformMatrix4fv(pUniform, false, new Float32Array(perspectiveMatrix.flatten()));
+    var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+    gl.uniformMatrix4fv(pUniform, false, new Float32Array(perspectiveMatrix.flatten()));
 
-  var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
-  gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
+    var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+    gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
 }
 
 window.onload = function() {
-	start();
+    start();
 }
--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 (function() {
-	'use strict';
+    'use strict';
 
-	var tabs,
-		displayDiv,
-		world,
-		aboutText,
-		home;
+    var tabs,
+        displayDiv,
+        world,
+        aboutText,
+        home;
 
-	function start() {
-		init();
-	}
-	
-	function init() {
-		tabs = document.getElementsByClassName('tabs');
-		displayDiv = document.getElementsByClassName('container');
-		world = document.getElementsByClassName('center');
-		home = displayDiv[0].innerHTML;
-		aboutText = document.createTextNode("I am recent BEng in Electronic and Engineering graduate currently working as a Technology Consultant.");
+    function start() {
+        init();
+    }
 
-		tabs[0].addEventListener('click', function(e) {
-			if(e.target.innerText === "About"){
-				world[0].style.visibility = "hidden";
-				displayDiv[0].appendChild(aboutText);
-			}
-			if(e.target.innerText === "Home"){
-				world[0].style.visibility = "visible";
-				aboutText.parentNode.removeChild(aboutText);
-			}
-		})
-	}
-	
-	start();
+    function init() {
+        tabs = document.getElementsByClassName('tabs');
+        displayDiv = document.getElementsByClassName('container');
+        world = document.getElementsByClassName('center');
+        home = displayDiv[0].innerHTML;
+        aboutText = document.createTextNode("I am recent BEng in Electronic and Engineering graduate currently working as a Technology Consultant.");
+
+        tabs[0].addEventListener('click', function(e) {
+            if (e.target.innerText === "About") {
+                world[0].style.visibility = "hidden";
+                displayDiv[0].appendChild(aboutText);
+            }
+            if (e.target.innerText === "Home") {
+                world[0].style.visibility = "visible";
+                aboutText.parentNode.removeChild(aboutText);
+            }
+        })
+    }
+
+    start();
 })();;
